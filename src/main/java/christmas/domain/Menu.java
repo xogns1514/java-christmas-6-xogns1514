3b22c3f79f23fd2f20f1a0af4b01f65846721816
taehoon <xogns1514@gmail.com>
2023-11-14 20:48:51 +0900

    feat(Menu): 같은 메뉴 판단 정의
    - 이름과 가격이 같으면 같은 메뉴로 판단한다.

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -1,5 +1,7 @@
 package christmas.domain;
 
+import java.util.Objects;
+
 public class Menu {
 
     private final String name;
@@ -22,5 +24,18 @@ public class Menu {
 
     public MenuCategory getCategory() {
         return category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Menu menu = (Menu) o;
+        return price == menu.price && Objects.equals(name, menu.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
     }
 }
